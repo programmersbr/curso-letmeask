1634977e4e59ce1113c3b0aac7d2f3a87ac4cdbf
EDUARDO SILVA <educwb@gmail.com>
2021-09-25 18:25:13 -0300

    arquivos deletados
    deletando os arquivos que são criados no comando create

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import '.services/firebase';
+import './services/firebase';
 
 ReactDOM.render(
   <React.StrictMode>
