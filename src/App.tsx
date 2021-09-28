10ad99e2102dea90e7c934cc7370c2aa68fa5246
Tauã Mendonça <tauamendonca@gmail.com>
2021-09-28 18:46:16 -0300

    Aula 2 - 46h30min

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import React from 'react';
+import { NewRoom } from './pages/NewRoom';
 
 function App() {
   return (
-    <div className="App">
-        Hello World
-    </div>
+    <NewRoom />
   );
 }
 
